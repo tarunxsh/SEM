0e3d50b2307a8c8ec8e36c111b4e4a9f64f41ede
tarunesh1234 <taruneshkumar1234@gmail.com>
2021-04-22 04:57:06 +0530

    heroku deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn SEM.wsgi --log-file -
--- SEM/settings.py
+++ SEM/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,3 +123,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_PASSWORD_VALIDATORS=[]
+django_heroku.settings(locals())
