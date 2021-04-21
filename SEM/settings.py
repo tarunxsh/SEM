3a599bfe77aa1aaa52a2544ec2854932be03d9a8
tarunesh1234 <taruneshkumar1234@gmail.com>
2021-04-22 05:08:15 +0530

    deploy

--- SEM/settings.py
+++ SEM/settings.py
@@ -121,6 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 AUTH_PASSWORD_VALIDATORS=[]
 django_heroku.settings(locals())
